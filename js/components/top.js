6200cc3e5800c70b3c188ad6d0b75da831236f08
orange <orange@mochen.com>
2018-03-27 16:25:37 +0800

    头部

--- js/components/top.js
+++ js/components/top.js
@@ -1,17 +1,17 @@
 Vue.component('lottery-top', {
     template: '#lottery-top',
-    props: ['lotteryConfig','lotteryCode','currentIssue','countTime','openIssue','lotteryType'],
-    data() {
+    props: ['lottery-config', 'lottery-type', 'lottery-code', 'current-issue', 'count-time', 'open-issue', 'open-code'],
+    data() {console.log(this)
         return {
             openCodeClassObject: {
-                'open-code-ssc': lotteryType === 'ssc',
-                'open-code-11x5': lotteryType === '11x5',
-                'open-code-pk10': lotteryType === 'pk10',
-                'open-code-k3': lotteryType === 'k3',
-                'open-code-3d': lotteryType === '3d',
-                'open-code-kl12': lotteryType === 'kl12',
-                'open-code-ky481': lotteryType === 'ky481',
-                'open-code-lhc': lotteryType === 'lhc',
+                'open-code-ssc': this.lotteryType === 'ssc',
+                'open-code-11x5': this.lotteryType === '11x5',
+                'open-code-pk10': this.lotteryType === 'pk10',
+                'open-code-k3': this.lotteryType === 'k3',
+                'open-code-3d': this.lotteryType === '3d',
+                'open-code-kl12': this.lotteryType === 'kl12',
+                'open-code-ky481': this.lotteryType === 'ky481',
+                'open-code-lhc': this.lotteryType === 'lhc',
             }
         };
     },
@@ -20,7 +20,24 @@ Vue.component('lottery-top', {
     beforeMount() {},
     mounted() {},
     computed: {
-
+        hours() {
+            let hours = Math.floor(this['countTime'] / 60 / 60);
+            hours = hours > 99 ? 99 : hours;
+            return hours;
+        },
+        minutes() {
+            let minutes = Math.floor(this['countTime'] / 60);
+            minutes = minutes < 10 ? `0{minutes}` : minutes;
+            return minutes;
+        },
+        seconds() {
+            let seconds = this['countTime'] % 60;
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+            return seconds;
+        },
+        openCodeArr() {
+            return this['openCode'].split(',');
+        }
     },
     watch: {},
     methods: {
